30ad050d68121cf321e9074244b541b2d5725ca6
Josh Hall <unstableplazma@gmail.com>
2025-09-02 18:15:08 +0800

    Change url

--- scripts/verify_success.js
+++ scripts/verify_success.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(arg) {
     const searchParams = new URLSearchParams(window.location.search);
     const id = btoa(searchParams.get("email"));
 
-    const url = "http://192.168.0.70:5000/weddings/" + id;
+    const url = "https://plaching.plazmasoftware.com/weddings/" + id;
 
     document.getElementById("wedding-link").href = document.getElementById("wedding-link").textContent = url;
     document.getElementById("qr-img").src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + url;
